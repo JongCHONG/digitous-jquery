518ff427b15ccb887616cb0f1b69518cc475905e
Yon Txpmdroflol <jochong27@gmail.com>
2021-10-18 16:13:20 +0200

    Exercice 30

--- exercice30/js/main.js
+++ exercice30/js/main.js
@@ -2,9 +2,10 @@ $(function() {
     $("#exercise input").keyup(function() {
         if ($("#exercise input").val().length > 5) {
             $("#exercise input").addClass("is-valid")
+            $("#exercise input").removeClass("is-invalid")
         } else {
             $("#exercise input").addClass("is-invalid")
-
+            $("#exercise input").removeClass("is-valid")
         }
     })
     
